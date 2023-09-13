9efcdb40418da3f0c85688b1f876d4704eae3697
bolaji Adeladan <bolaji.adeladan@gmail.com>
2023-09-13 07:44:27 +0100

    starter setup

---
+++ lib/constants/text_constants.dart
@@ -0,0 +1,6 @@
+class AppTexts {
+  static const String fullName = 'Adeladan Oluwaseun Bolaji';
+  static const String slackName = 'bolajibillionz';
+  static const String gitHubHandle = 'bolajibillionz';
+ 
+}
